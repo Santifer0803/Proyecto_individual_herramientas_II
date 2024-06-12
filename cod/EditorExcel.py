986ffe95ffb56a8298e6fec46b3684a4b2ca4adf
Santifer0803 <santifer0810@gmail.com>
2024-06-12 11:11:40 -0600

    docs: Documentación de la clase EditorExcel del módulo EditorExcel

--- cod/EditorExcel.py
+++ cod/EditorExcel.py
@@ -9,9 +9,67 @@ import xlsxwriter
 import pandas as pd
 
 class EditorExcel:
+    """
+    Clase que representa crea un documento de Excel y lo modifica, iniciando en un DataFrame
+
+    Attributes
+    ----------
+    escritor : pandas.io.excel._xlsxwriter.XlsxWriter
+        Elemento que permite editar el Excel
+    libro : xlsxwriter.workbook.Workbook
+        Elemento que define el libro de Excel en el que se trabajará
+    hoja : xlsxwriter.worksheet.Worksheet
+        Elemento que define la hoja a trabajar en el Excel
+
+    Methods
+    -------
+    cerrar(self):
+        Método que cierra el editor, es necesario usarlo siempre que se termina de editar un archivo, de lo contrario el Excel no podrá ser abierto. En caso de
+        no aplicar este método al finalizar las ediciones del Excel, se recomienda reiniciar el kernel
+        
+    escribir(self, texto, formato_escritura = 'python', fila = 0, columna = 0, celda = 'A1'):
+        Método que permite escribir texto, números o fórmulas básicas en una celda de excel
+        
+    formula(self, texto, formato_escritura = 'python', fila_ini = 0, columna_ini = 0, fila_fin = 0, columna_fin = 0, celda = 'A1:A1'):
+        Método que permite escribir fórmulas de Excel en una o varias casillas, las mismas deben de escribirse en inglés sin importar el idioma del la
+        computadora o del Excel
+        
+    escribir_fila(self, lista, formato_escritura = 'python', fila = 0, columna = 0, celda = 'A1'):
+        Método que permite poner una lista de python, en una fila de Excel
+        
+    escribir_columna(self, lista, formato_escritura = 'python', fila = 0, columna = 0, celda = 'A1'):
+        Método que permite poner una lista de python, en una columna de Excel
+        
+    autoajuste(self):
+        Método que permite autoajustar todas las columnas que tengan algo escrito en el documento
     
+    condicional(self, condicion, valor, formato_escritura = 'python', fila_ini = 0, columna_ini = 0, fila_fin = 0, columna_fin = 0, celdas = 'A1:A1'):
+        Método que permite poner formato condicional en un rango de casillas de Excel
+    
+    mini_barras(self, celda_grafico, celda_datos):
+        Método que permite crear un gráfico de barras pequeño (sparklines), con base en una fila o columna concreta
+    
+    filtro(self, formato_escritura = 'python', fila_ini = 0, columna_ini = 0, fila_fin = 0, columna_fin = 0, celdas = 'A1:B1'):
+        Método que agrega el autofiltro a una selección de celdas de Excel
+    
+    proteger(self, contraseña, formato = True, eliminar_filas = True, eliminar_columnas = True):
+        Método que proteje la hoja, permite elegir si el usuario puede alterar ciertos objetos con una contraseña
+    """
     # Constructor
     def __init__(self, df, nombre):
+        ''' Constructor de la clase EditorExcel
+        
+        Parameters
+        ---------
+        df : pandas.core.DataFrame
+            DataFrame a convertir en archivo de Excel
+        nombre : str
+            Cadena con el nombre del archivo, debe terminar en .xlsx y no llevar espacios
+        
+        Returns
+        -----
+        None
+        '''
         self.__escritor = pd.ExcelWriter(nombre, engine = 'xlsxwriter')
         
         # Se convierte el DataFrame a un
@@ -24,27 +82,93 @@ class EditorExcel:
     # Get y set
     @property
     def escritor(self):
+        ''' Método get de la clase EditorExcel, atributo escritor
+        
+        Parameters
+        ---------
+        None
+        
+        Returns
+        -----
+        escritor : pandas.io.excel._xlsxwriter.XlsxWriter
+            Elemento que permite editar el Excel
+        '''
         return self.__escritor
     
     @property
     def libro(self):
+        ''' Método get de la clase EditorExcel, atributo libro
+        
+        Parameters
+        ---------
+        None
+        
+        Returns
+        -----
+        libro : xlsxwriter.workbook.Workbook
+            Elemento que define el libro de Excel en el que se trabajará
+        '''
         return self.__libro
     
     @property
     def hoja(self):
+        ''' Método get de la clase EditorExcel, atributo hoja
+        
+        Parameters
+        ---------
+        None
+        
+        Returns
+        -----
+        hoja : xlsxwriter.worksheet.Worksheet
+            Elemento que define la hoja a trabajar en el Excel
+        '''
         return self.__hoja
     
     @escritor.setter
     def escritor(self, nuevo_escritor):
+        ''' Método set de la clase EditorExcel, atributo escritor
+        
+        Parameters
+        ---------
+        nuevo_escritor : pandas.io.excel._xlsxwriter.XlsxWriter
+            Elemento que permite redefinir el editar el Excel
+        
+        Returns
+        -----
+        None
+        '''
         self.__escritor = nuevo_escritor
     
     @libro.setter
     def libro(self, nuevo_libro):
+        ''' Método set de la clase EditorExcel, atributo libro
+        
+        Parameters
+        ---------
+        nuevo_libro : xlsxwriter.workbook.Workbook
+            Elemento que permite redefinir el libro en el que se trabaja
+        
+        Returns
+        -----
+        None
+        '''
         self.__libro = nuevo_libro
     
     @hoja.setter
-    def hoja(self, nuevo_hoja):
-        self.__hoja = nuevo_hoja
+    def hoja(self, nueva_hoja):
+        ''' Método set de la clase EditorExcel, atributo hoja
+        
+        Parameters
+        ---------
+        nueva_hoja : xlsxwriter.worksheet.Worksheet
+            Elemento que permite redefinir la hoja en la que se trabaja
+        
+        Returns
+        -----
+        None
+        '''
+        self.__hoja = nueva_hoja
         
     # str
     def __str__(self):
@@ -107,7 +231,8 @@ class EditorExcel:
             return print('El formato de escritura no es válido')
     
     def formula(self, texto, formato_escritura = 'python', fila_ini = 0, columna_ini = 0, fila_fin = 0, columna_fin = 0, celda = 'A1:A1'):
-        ''' Método que permite escribir fórmulas de Excel en una o varias casillas
+        ''' Método que permite escribir fórmulas de Excel en una o varias casillas, las mismas deben de escribirse en inglés sin importar el idioma del la
+            computadora o del Excel
             
         Parameters
         ---------
@@ -257,7 +382,7 @@ class EditorExcel:
             return print('El formato de escritura no es válido')
     
     def mini_barras(self, celda_grafico, celda_datos):
-        ''' Método que permite crear un gráfico de barras pequeño (sparklines)
+        ''' Método que permite crear un gráfico de barras pequeño (sparklines), con base en una fila o columna concreta
             
         Parameters
         ---------
@@ -310,11 +435,11 @@ class EditorExcel:
         contraseña : str
             Contraseña que el usuario debe poner para modificar las opciones protegidas
         formato : bool
-            Decide si se protege el formato de las celdas
+            Decide si se protege el formato de las celdas, True por defecto
         eliminar_filas : bool
-            Decide si se protege la eliminación de las filas del Excel
+            Decide si se protege la eliminación de las filas del Excel, True por defecto
         eliminar_columnas : bool
-            Decide si se protege la eliminación de las columnas del Excel
+            Decide si se protege la eliminación de las columnas del Excel, True por defecto
         
         Returns
         -----
